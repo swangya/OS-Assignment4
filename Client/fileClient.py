e0df3a364c3e4cec09d5aa7b97fc583f6da5c6a0
Swangya Saurav <swangya@gmail.com>
2016-12-09 21:53:58 -0600

    Encryption Working

--- Client/fileClient.py
+++ Client/fileClient.py
@@ -1,13 +1,76 @@
 import socket
-import os
+import os, random
+
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+
+def encrypt(key, filename):
+	chunksize = 64*1024
+	outputFile = "(encrypted)"+filename
+	filesize = str(os.path.getsize(filename)).zfill(16)
+	IV = ''
+
+	for i in range(16):
+		IV += chr(random.randint(0, 0xFF))
+
+	encryptor = AES.new(key, AES.MODE_CBC, IV)
+
+	with open(filename, 'rb') as infile:
+		with open(outputFile, 'wb') as outfile:
+			outfile.write(filesize)
+			outfile.write(IV)
+
+			while True:
+				chunk = infile.read(chunksize)
+
+				if len(chunk) == 0:
+					break
+				elif len(chunk) % 16 != 0:
+					chunk += ' ' * (16 - (len(chunk) % 16))
+
+				outfile.write(encryptor.encrypt(chunk))
+
+
+def decrypt(key, filename):
+	chunksize = 64*1024
+	outputFile = filename[11:]
+
+	with open(filename, 'rb') as infile:
+		filesize = long(infile.read(16))
+		IV = infile.read(16)
+
+		decryptor = AES.new(key, AES.MODE_CBC, IV)
+
+		with open(outputFile, 'wb') as outfile:
+			while True:
+				chunk = infile.read(chunksize)
+
+				if len(chunk) == 0:
+					break
+
+				outfile.write(decryptor.decrypt(chunk))
+			outfile.truncate(filesize)
+
+
+def getKey(password):
+	hasher = SHA256.new(password)
+	return hasher.digest()
+
 
 def sendFile(s):
     filename = raw_input("Filename? -> ")
+    password = raw_input("Password? -> ")
+
+    encrypt(getKey(password), filename)
+
+    filename = "(encrypted)"+filename
+
     if filename != 'q':
         s.send(filename)
     else:
         exit();
     if os.path.isfile(filename):
+        print "Sending data: " + str(os.path.getsize(filename))
         s.send(str(os.path.getsize(filename)))
         with open(filename, 'rb') as f:
                 bytesToSend = f.read(1024)
--- Server/fileServer.py
+++ Server/fileServer.py
@@ -4,19 +4,21 @@ import os
 def getFile(sock):
     filename = sock.recv(1024)
     data = sock.recv(1024)
+    print "Lenth received is: " + data
     filesize = long(data)
     f = open(filename, 'wb')
-    data = sock.recv(1024)
-    totalRecv = len(data)
-    f.write(data)
+    data1 = sock.recv(1024)
+    totalRecv = len(data1)
+    f.write(data1)
     while totalRecv < filesize:
-        data = sock.recv(1024)
-        totalRecv += len(data)
-        f.write(data)
+        data1 = sock.recv(1024)
+        totalRecv += len(data1)
+        f.write(data1)
         print "{0:.2f}".format((totalRecv/float(filesize))*100)+ "% Done"
     print("FILE RECEIVED")
     f.close()
-
+    data = None
+    data1 = None
     sock.close();
 
 def sendFile(sock):
